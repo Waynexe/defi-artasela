7271f4c220e43cf9b1937c79d96623dd9cef98fc
tuahiv_m <tuahiv_m@etna-alternance.net>
2022-04-08 19:20:20 -0400

    maj

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://artasela-node.herokuapp.com/",
   headers: {
     "Content-type": "application/json"
   }
